57ca2021acbefd352615d0e6a9126de9a138bbd3
Holutunde <Holutunde99@gmail.com>
2022-08-06 01:36:17 +0100

    admin get all users

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,6 +1,9 @@
 const User = require('../models/userSchema')
 
-const getAllUsers = async (req, res) => {}
+const getAllUsers = async (req, res) => {
+  const users = await User.find({}).sort('createdAt')
+  res.status(200).json({ allUser: users })
+}
 
 const getUser = async (req, res) => {}
 
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,7 +7,27 @@ const register = async (req, res) => {
   res.status(200).json({ user: { name: newUser.username }, newToken })
 }
 
-const login = async (req, res) => {}
+const login = async (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(400).json('Please provide email and password')
+  }
+  const getUser = await User.findOne({ email })
+  if (!getUser) {
+    return res.status(401).json('Invalid Credentials')
+  }
+
+  const samePassword = await getUser.confirmPassword(password)
+  if (!samePassword) {
+    return res.status(401).json('Invalid Credentials')
+  }
+
+  const userToken = getUser.createWebToken()
+  res.status(200).json({
+    user: `username: ${getUser.username} has logged in`,
+    userToken,
+  })
+}
 
 module.exports = {
   register,
